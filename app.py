2b1eee639803b8a0dc3eb64f0329561379a2b859
TomHearmon2001 <c0037681@newcastle.ac.uk>
2023-02-21 17:06:37 +0100

    Multi Domain Operations Query

--- app.py
+++ app.py
@@ -26,89 +26,95 @@ class NationForm(FlaskForm):
     nationResult = MultiCheckboxField('Label', choices=files)
 
 
-class ExersiceFrom(FlaskForm):
-    string_of_exersices = ['CWIX_2021\r\nCWIX_2022\r\n']
-    list_of_exersices = string_of_exersices[0].split()
-    # create a list of value/description tuples
-    files = [(x, x) for x in list_of_exersices]
-    exersiceResult = MultiCheckboxField('Label', choices=files)
-
-
-class TaskFrom(FlaskForm):
-    string_of_tasks = ['Data_Centric_Security_Approach\r\nConformance_Testing\r\nDe-Risking_Coalition_Operations\r\n'
-                       'Data_Analytics\r\nConnecting_sensor_-_decision_maker_effector\r\n'
-                       'Federated_Mission_Networking\r\nMulti-Domain_Operations\r\nStandards_Violation\r\n']
-    list_of_tasks = string_of_tasks[0].split()
-    # create a list of value/description tuples
-    files = [(x, x) for x in list_of_tasks]
-    taskResult = MultiCheckboxField('Label', choices=files)
-
-
-class DomainFrom(FlaskForm):
-    string_of_domains = ['Air\nLand\r\nMaritime\r\nCyberspace\r\nSpace\r\nOther_Support_Services\r\n']
-    list_of_domains = string_of_domains[0].split()
-    # create a list of value/description tuples
-    files = [(x, x) for x in list_of_domains]
-    domainResult = MultiCheckboxField('Label', choices=files)
-
-
-class WarfareFrom(FlaskForm):
-    string_of_warfare = ['Strategic\nOperational\r\nDeployed\r\n']
-    list_of_warfare = string_of_warfare[0].split()
-    # create a list of value/description tuples
-    files = [(x, x) for x in list_of_warfare]
-    warfareResult = MultiCheckboxField('Label', choices=files)
-
-
-class TableForm(FlaskForm):
-    string_of_tables = ['Capabilities\r\n']
-    list_of_tables = string_of_tables[0].split()
-    # create a list of value/description tuples
-    files = [(x, x) for x in list_of_tables]
-    tableResult = MultiCheckboxField('Label', choices=files)
+# class ExersiceFrom(FlaskForm):
+#     string_of_exersices = ['CWIX_2021\r\nCWIX_2022\r\n']
+#     list_of_exersices = string_of_exersices[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_exersices]
+#     exersiceResult = MultiCheckboxField('Label', choices=files)
+#
+#
+# class TaskFrom(FlaskForm):
+#     string_of_tasks = ['Data_Centric_Security_Approach\r\nConformance_Testing\r\nDe-Risking_Coalition_Operations\r\n'
+#                        'Data_Analytics\r\nConnecting_sensor_-_decision_maker_effector\r\n'
+#                        'Federated_Mission_Networking\r\nMulti-Domain_Operations\r\nStandards_Violation\r\n']
+#     list_of_tasks = string_of_tasks[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_tasks]
+#     taskResult = MultiCheckboxField('Label', choices=files)
+#
+#
+# class DomainFrom(FlaskForm):
+#     string_of_domains = ['Air\nLand\r\nMaritime\r\nCyberspace\r\nSpace\r\nOther_Support_Services\r\n']
+#     list_of_domains = string_of_domains[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_domains]
+#     domainResult = MultiCheckboxField('Label', choices=files)
+#
+#
+# class WarfareFrom(FlaskForm):
+#     string_of_warfare = ['Strategic\nOperational\r\nDeployed\r\n']
+#     list_of_warfare = string_of_warfare[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_warfare]
+#     warfareResult = MultiCheckboxField('Label', choices=files)
+#
+#
+# class TableForm(FlaskForm):
+#     string_of_tables = ['Capabilities\r\n']
+#     list_of_tables = string_of_tables[0].split()
+#     # create a list of value/description tuples
+#     files = [(x, x) for x in list_of_tables]
+#     tableResult = MultiCheckboxField('Label', choices=files)
 
 
 @app.route('/', methods=['post', 'get'])
-def hello_world():
+def multi_Domain_Operations_By_Nation():
     # Initiate Program
-    table_form = TableForm()
     nation_form = NationForm()
-    exersice_form = ExersiceFrom()
-    task_form = TaskFrom()
-    domain_form = DomainFrom()
-    warfare_form = WarfareFrom()
-
-    if table_form.validate_on_submit():
-        print(table_form.tableResult.data)
-        if table_form.tableResult.data == "Capabilities":
-            return render_template("capabilites.html")
-
+    query = "select t.name, c.name AS CAPABILITY_NAME, n.name AS NATION_NAME from tasks t inner join capability_tasks "\
+            "ct on t.id = ct.task_id inner join capabilities c on c.id = ct.capability_id inner join nations n on "\
+            "n.id = c.nation_id where t.id = 8 and n.id = "
+    # Form Validation
+    if nation_form.validate_on_submit():
+        print(nation_form.nationResult.data)
+        complete_query = query + ''.join(nation_form.nationResult.data)
+        return render_template("result.html", nationData=nation_form.nationResult.data, completeQuery=complete_query)
     else:
         print("Validation Failed")
-        print("Table Form Errors ", table_form.errors)
-    return render_template('table.html', tableForm=table_form)
+        print("Nation Form Errors ", nation_form.errors)
 
+    return render_template('search.html', nationForm=nation_form)
 
-    # # Form Validation
-    # if nation_form.validate_on_submit() & exersice_form.validate_on_submit() & task_form.validate_on_submit() & \
-    #         domain_form.validate_on_submit() & warfare_form.validate_on_submit():
-    #     print(nation_form.nationResult.data)
-    #     print(exersice_form.exersiceResult.data)
-    #     print(task_form.taskResult.data)
-    #     print(domain_form.domainResult.data)
-    #     print(warfare_form.warfareResult.data)
-    #     return render_template("result.html", nationData=nation_form.nationResult.data,
-    #                            exersiceData=exersice_form.exersiceResult.data, taskData=task_form.taskResult.data,
-    #                            domainData=domain_form.domainResult.data, warfareData=warfare_form.warfareResult.data)
-    # else:
-    #     print("Validation Failed")
-    #     print("Nation Form Errors ", nation_form.errors)
-    #     print("Exersice Form Errors ", exersice_form.errors)
-    #     print("Task Form Errors ", task_form.errors)
-    #     print("Domain Form Errors ", domain_form.errors)
-    #     print("Warfare Form Errors ", warfare_form.errors)
-    # return render_template('search.html', nationForm=nation_form, exersiceForm=exersice_form, taskForm=task_form,
-    #                        domainForm=domain_form, warfareForm=warfare_form)
+# @app.route('/', methods=['post', 'get'])
+# def hello_world():
+#     # Initiate Program
+#     nation_form = NationForm()
+#     exersice_form = ExersiceFrom()
+#     task_form = TaskFrom()
+#     domain_form = DomainFrom()
+#     warfare_form = WarfareFrom()
+#
+#     # Form Validation
+#     if nation_form.validate_on_submit() & exersice_form.validate_on_submit() & task_form.validate_on_submit() & \
+#             domain_form.validate_on_submit() & warfare_form.validate_on_submit():
+#         print(nation_form.nationResult.data)
+#         print(exersice_form.exersiceResult.data)
+#         print(task_form.taskResult.data)
+#         print(domain_form.domainResult.data)
+#         print(warfare_form.warfareResult.data)
+#         return render_template("result.html", nationData=nation_form.nationResult.data,
+#                                exersiceData=exersice_form.exersiceResult.data, taskData=task_form.taskResult.data,
+#                                domainData=domain_form.domainResult.data, warfareData=warfare_form.warfareResult.data)
+#     else:
+#         print("Validation Failed")
+#         print("Nation Form Errors ", nation_form.errors)
+#         print("Exersice Form Errors ", exersice_form.errors)
+#         print("Task Form Errors ", task_form.errors)
+#         print("Domain Form Errors ", domain_form.errors)
+#         print("Warfare Form Errors ", warfare_form.errors)
+#     return render_template('search.html', nationForm=nation_form, exersiceForm=exersice_form, taskForm=task_form,
+#                            domainForm=domain_form, warfareForm=warfare_form)
 
 
 if __name__ == '__main__':
